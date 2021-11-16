a4bd769164ea45f697cb7e9ae87deabadf4e7265
ono-teruya1 <27873650+teru01@users.noreply.github.com>
2021-11-16 21:50:34 +0900

    add read and post

--- go.mod
+++ go.mod
@@ -1,5 +1,12 @@
-module fgocat
+module fluent-cat-go
 
 go 1.16
 
-require github.com/urfave/cli v1.22.5
+require (
+	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
+	github.com/fluent/fluent-logger-golang v1.7.0
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/tinylib/msgp v1.1.6 // indirect
+	github.com/urfave/cli/v2 v2.3.0
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1
+)
--- main.go
+++ main.go
@@ -1,25 +1,83 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
-	"github.com/urfave/cli"
+	"github.com/fluent/fluent-logger-golang/fluent"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/xerrors"
 )
 
 func main() {
 	app := &cli.App{
-		Name:  "boom",
-		Usage: "make an explosive entrance",
-		Action: func(c *cli.Context) error {
-			fmt.Println("boom! I say!")
-			return nil
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "tag",
+				Aliases: []string{"t"},
+				Value:   "",
+				Usage:   "tag of fluentd",
+			},
+			&cli.StringFlag{
+				Name:    "host",
+				Aliases: []string{"h"},
+				Value:   "127.0.0.1",
+				Usage:   "destination host",
+			},
+			&cli.StringFlag{
+				Name:    "port",
+				Aliases: []string{"p"},
+				Value:   "",
+				Usage:   "destination port",
+			},
 		},
+		Name:   "fluent-cat-go",
+		Usage:  "fluent-cat-go -h [HOST] -p [PORT]",
+		Action: readAndPost,
 	}
 
 	err := app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("%+v\n", err)
+		os.Exit(1)
 	}
+}
+
+func readAndPost(c *cli.Context) error {
+	logger, err := fluent.New(fluent.Config{})
+	if err != nil {
+		return xerrors.Errorf("failed to create fluent logger: %w", err)
+	}
+	defer logger.Close()
+	tag := "myapp.access"
+
+	reader := bufio.NewReaderSize(os.Stdin, 2<<18)
+	for {
+		line, isPrefix, err := reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Println("failed to read line", err)
+			continue
+		}
+		if isPrefix {
+			log.Println("warn: msg too long")
+			continue
+		}
+		payload := make(map[string]interface{})
+		if err = json.Unmarshal(line, &payload); err != nil {
+			log.Println("failed to parse json", err)
+			continue
+		}
+		if err := logger.Post(tag, payload); err != nil {
+			log.Println("failed to post", err)
+			continue
+		}
+	}
+	return nil
 }
